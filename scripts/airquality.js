ec10c07b2917069e3fcb3437936bb720cd53bd31
jreuel64 <long3522@stthomas.edu>
2019-04-12 21:16:40 -0500

    update

--- scripts/airquality.js
+++ scripts/airquality.js
@@ -1,10 +1,58 @@
 var app;
-var auth_data = {};
+
+var map1;
+var map2;
 
 function Init()
 {
-    //map1 is id of div to place in
-    var map1 = L.map("map1").setView([51.505, -.09], 13); //([lat, long], zoom level)
-    //var map2 = L.map("map2").setView([51.505, -.09], 13);
+    app = new Vue({
+        el: "#app",
+        data: {
+            map1_lat: 44.9537,
+            map1_long: -93.09, 
+            map2_lat: 44.9537,
+            map2_long: -93.09,
+            title: "Air Quality App"
+                
+        }
+    });
 
+    map1 = L.map("map1").setView([app.map1_lat, app.map1_long], 13); //([lat, long], zoom level)
+    map2 = L.map("map2").setView([51.505, -.09], 13);
+
+
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 16,
+    minZoom: 8,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoianJldWVsNjQiLCJhIjoiY2p1ZXMzN3A3MDYyYzQ1bW84Ymcyb2kwMyJ9.PDaEm5xuepXw-U8dtvEAvw'
+    }).addTo(map1);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 16,
+    minZoom: 8,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoianJldWVsNjQiLCJhIjoiY2p1ZXMzN3A3MDYyYzQ1bW84Ymcyb2kwMyJ9.PDaEm5xuepXw-U8dtvEAvw'
+    }).addTo(map2);
+
+}
+
+function loadNewLocations()
+{
+    console.log("loading in new locations");
+}
+
+
+
+function reloadMap1View()
+{
+    map1.setView([app.map1_lat, app.map1_lat]);
+}
+
+function reloadMap2View()
+{
+    map2.setView([app.map2_lat, app.map2_lat]);
 }
