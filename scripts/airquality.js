af9473356cd0378bb5cc97e9abad9e624e4cee72
jreuel64 <long3522@stthomas.edu>
2019-04-14 12:54:50 -0500

    marker creation

--- scripts/airquality.js
+++ scripts/airquality.js
@@ -20,12 +20,10 @@ function Init()
     map1 = L.map("map1").setView([app.map1_lat, app.map1_long], 13); //([lat, long], zoom level)
     map2 = L.map("map2").setView([app.map2_lat, app.map2_long], 13);
 
-
-
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 16,
-    minZoom: 8,
+    minZoom: 9,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoianJldWVsNjQiLCJhIjoiY2p1ZXMzN3A3MDYyYzQ1bW84Ymcyb2kwMyJ9.PDaEm5xuepXw-U8dtvEAvw'
     }).addTo(map1);
@@ -33,7 +31,7 @@ function Init()
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 16,
-    minZoom: 8,
+    minZoom: 9,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoianJldWVsNjQiLCJhIjoiY2p1ZXMzN3A3MDYyYzQ1bW84Ymcyb2kwMyJ9.PDaEm5xuepXw-U8dtvEAvw'
     }).addTo(map2);
@@ -46,7 +44,6 @@ function Init()
         //console.log("lat: " + lat + " long: " + long);
         app.map1_lat=lat.toFixed(5);
         app.map1_long=long.toFixed(5);
-
     });
 
     map2.on("move", function() {
@@ -60,7 +57,7 @@ function Init()
     });
 
     loadAirData(1);
-
+    loadAirData(2);
 }
 
 function mapSubmitLongLat(mapNum)
@@ -126,6 +123,11 @@ function loadAirData(mapNum)
     var corner2;
     var radius;
     var url;
+    var date;
+
+    date = new Date();
+    date.setDate(date.getDate() - 30);
+    console.log(date);
 
     if(mapNum == 1)
     {
@@ -148,14 +150,13 @@ function loadAirData(mapNum)
 
     console.log("radius: " + radius);
 
-    url = "https://api.openaq.org/v1/latest?coordinates=" + center.lat + "," + center.lng + "&radius=" + radius;
+    url = "https://api.openaq.org/v1/measurements?coordinates=" + center.lat + "," + center.lng + "&radius=" + radius + "&order_by[]=location&order_by[]=date&sort[]=asc&sort[]=desc&limit=10000&date_from=" + date;
 
     console.log(url);
 
     $.getJSON(url, function(json) {
-        console.log(json);
+        populateMarkers(mapNum, json);
     });
-
 }
 
 function getRadius(corner1, corner2)
@@ -178,6 +179,79 @@ function getRadius(corner1, corner2)
     var radius = (6371000 * c) / 2;
 
     return radius;
+}
+
+function populateMarkers(mapNum, json)
+{
+    console.log("populating Markers");
+    console.log(json);
+    var table;
+
+    if(mapNum == 1)
+    {
+        table = $("#map1table");
+    }
+    else
+    {
+        table = $("#map2table");
+    }
+    for(var i = 0; i < json.meta.found - 1; ++i)
+    {
+        var location = json.results[i].location;
+        var point = json.results[i].coordinates;
+        var locAverage = json.results[i].value;
+        var count = 0;
+
+        var row = $("<tr>");
+        var c1 = $("<td>").text(location);
+        var c2 = $("<td>").text(json.results[i].date.local.toString());
+        var c3 = $("<td>").text(json.results[i].value);
+
+        row.append(c1);
+        row.append(c2);
+        row.append(c3);
+
+        table.append(row);
+
+        //while at same location
+        while(i < json.meta.found - 1 && location == json.results[i+1].location)
+        {
+            locAverage = locAverage + json.results[i+1].value;
+         
+            var row = $("<tr>");
+            var c1 = $("<td>").text(location);
+            var c2 = $("<td>").text(json.results[i+1].date.local);
+            var c3 = $("<td>").text(json.results[i+1].value);
+
+            row.append(c1);
+            row.append(c2);
+            row.append(c3);
+
+            table.append(row);
+
+            ++i;
+            ++count;
+        }
+
+        locAverage = (locAverage / count).toFixed(2);
+        console.log(i + " " + locAverage);
+
+        if(mapNum == 1)
+        {
+            marker = L.marker([point.latitude, point.longitude]);
+            marker.bindTooltip(json.results[i].location + "</br> Average: <b>" + locAverage + "</b> micrograms / cubic meter").openTooltip();
+
+            marker.addTo(map1);
+        }
+        else
+        {
+            marker = L.marker([point.latitude, point.longitude]);
+            marker.bindTooltip(json.results[i].location + "</br> Average: <b>" + locAverage + "</b> micrograms / cubic meter").openTooltip();
+
+            marker.addTo(map2);
+
+        }
+    }
 }
 
 
