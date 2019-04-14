5f3ef1899148b7b28fd812f41b7aea42a2943149
jreuel64 <long3522@stthomas.edu>
2019-04-13 21:33:20 -0500

    drag update latlng working

--- scripts/airquality.js
+++ scripts/airquality.js
@@ -8,6 +8,8 @@ function Init()
     app = new Vue({
         el: "#app",
         data: {
+            map1_name: "Saint Paul",
+            map2_name: "Saint Paul",
             map1_lat: 44.9537,
             map1_long: -93.09, 
             map2_lat: 44.9537,
@@ -16,7 +18,7 @@ function Init()
     });
 
     map1 = L.map("map1").setView([app.map1_lat, app.map1_long], 13); //([lat, long], zoom level)
-    map2 = L.map("map2").setView([51.505, -.09], 13);
+    map2 = L.map("map2").setView([app.map2_lat, app.map2_long], 13);
 
 
 
@@ -36,21 +38,120 @@ function Init()
     accessToken: 'pk.eyJ1IjoianJldWVsNjQiLCJhIjoiY2p1ZXMzN3A3MDYyYzQ1bW84Ymcyb2kwMyJ9.PDaEm5xuepXw-U8dtvEAvw'
     }).addTo(map2);
 
+
+    map1.on("move", function() {
+        //console.log("map1 moved");
+        lat=map1.getCenter().lat;
+        long=map1.getCenter().lng;
+        //console.log("lat: " + lat + " long: " + long);
+        app.map1_lat=lat.toFixed(5);
+        app.map1_long=long.toFixed(5);
+
+    });
+
+    map2.on("move", function() {
+        //console.log("map1 moved");
+        lat=map2.getCenter().lat;
+        long=map2.getCenter().lng;
+        //console.log("lat: " + lat + " long: " + long);
+        app.map2_lat=lat.toFixed(5);
+        app.map2_long=long.toFixed(5);
+
+    });
+
 }
 
-function loadNewLocations()
+function mapSubmitLongLat(mapNum)
 {
-    console.log("loading in new locations");
+    console.log("Entered MapSubmitLongLat(mapNum)");
+    var lat;
+    var long;
+
+    if(mapNum == 1)
+    {
+        lat = $("#loc1Lat").val();
+        long = $("#loc1Long").val();
+
+        console.log("mapnum is 1");
+        console.log("lat: " + lat + " long: " + long);
+
+        if(latLongIsValid(lat, long))
+        {
+
+            app.map1_lat=lat;
+            app.map1_long=long;
+
+            reloadMapView(1);
+        }
+        else
+        {
+            alert("ERROR: Latitude or Longitude is out of range.");
+            //$("#loc1Lat").val("");
+            //$("#loc1Long").val("");
+        }
+    }
+    else
+    {
+        lat = $("#loc2Lat").val();
+        long = $("#loc2Long").val();
+
+        console.log("mapnum is 2");
+        console.log("lat: " + lat + " long: " + long);
+
+        if(latLongIsValid(lat, long))
+        {
+            app.map2_lat = lat;
+            app.map2_long = long;
+
+            reloadMapView(2);
+        }
+        else
+        {
+            alert("ERROR: Latitude or Longitude is out of range.");
+            //$("#loc2Lat").val("");
+            //$("#loc2Long").val("");
+        }
+    }
+}
+
+function mapMoved(mapNum)
+{
+    console.log("map " + mapNum + " was moved.");
+
+    if(mapNum == 1)
+    {
+        //console.log(map1.getCenter());
+    }
+    else
+    {
+        //console.log(map2.getCenter());
+    }
 }
 
 
 
-function reloadMap1View()
+function latLongIsValid(lat, long)
 {
-    map1.setView([app.map1_lat, app.map1_lat]);
+    if(lat > 90 || lat < -90)
+    {
+        return false;
+    }
+    if(long > 180 || long < -180)
+    {
+        return false;
+    }
+
+    else{return true;}
 }
 
-function reloadMap2View()
+function reloadMapView(mapNum)
 {
-    map2.setView([app.map2_lat, app.map2_lat]);
+    if(mapNum == 1)
+    {
+        map1.setView([app.map1_lat, app.map1_long]);
+    }
+    else
+    {
+        map2.setView([app.map2_lat, app.map2_long]);
+    }
 }
